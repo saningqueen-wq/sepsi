7052b210ca821c4e4ccffb9998f84a64a01a2f77
saningqueen-wq <saningqueen@gmail.com>
2026-08-16 00:03:25 -0600

    Corregir avatar de princesa en todos los comentarios

--- princesa-avatar-patch.js
+++ princesa-avatar-patch.js
@@ -1,32 +1,76 @@
 (function () {
     "use strict";
 
-    const PRINCESA_AVATAR = "img/15228476856ed85cdc6d585b92c87b27.jpg";
+    const PRINCESA_AVATAR = "img/15228476856ed85cdc6d585b92c87b27.jpg?v=3";
 
-    function applyPrincessAvatar() {
-        document.querySelectorAll("#comments .comment").forEach(function (comment) {
-            const name = comment.querySelector("strong");
-            const avatar = comment.querySelector(".comment-avatar");
+    function setPrincessAvatar(comment) {
+        if (!comment || comment.nodeType !== 1) {
+            return;
+        }
 
-            if (!name || !avatar || !/princesa/i.test(name.textContent || "")) {
-                return;
-            }
+        const name = comment.querySelector("strong, .comment-name, .author, .username");
+        if (!name || !/princesa/i.test(name.textContent || "")) {
+            return;
+        }
 
-            avatar.src = PRINCESA_AVATAR;
-            avatar.alt = "Avatar de princesa";
-            avatar.onerror = null;
-        });
+        const avatar = comment.querySelector("img.comment-avatar, img.blog-comment-avatar, img[class*='avatar'], img");
+        if (!avatar) {
+            return;
+        }
+
+        avatar.src = PRINCESA_AVATAR;
+        avatar.alt = "Avatar de princesa";
+        avatar.removeAttribute("srcset");
+        avatar.style.objectFit = "cover";
+        avatar.style.borderRadius = "50%";
+        avatar.onerror = function () {
+            this.onerror = null;
+            this.src = "img/15228476856ed85cdc6d585b92c87b27.jpg?v=3";
+        };
+    }
+
+    function applyPrincessAvatar(root) {
+        const scope = root && root.querySelectorAll ? root : document;
+
+        if (scope.matches && scope.matches(".comment, .blog-comment, [class*='comment']")) {
+            setPrincessAvatar(scope);
+        }
+
+        scope.querySelectorAll(".comment, .blog-comment, [class*='comment']").forEach(setPrincessAvatar);
 
         document.querySelectorAll("#wiki .wiki-card:not(.new-wiki) img").forEach(function (image) {
             image.src = PRINCESA_AVATAR;
             image.alt = "Imagen de princesa";
+            image.removeAttribute("srcset");
             image.onerror = null;
         });
     }
 
+    function startObserver() {
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                mutation.addedNodes.forEach(function (node) {
+                    if (node.nodeType === 1) {
+                        applyPrincessAvatar(node);
+                    }
+                });
+            });
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+
+    function init() {
+        applyPrincessAvatar(document);
+        startObserver();
+    }
+
     if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", applyPrincessAvatar, { once: true });
+        document.addEventListener("DOMContentLoaded", init, { once: true });
     } else {
-        applyPrincessAvatar();
+        init();
     }
 })();
